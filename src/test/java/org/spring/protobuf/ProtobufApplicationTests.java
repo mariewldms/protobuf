e69793cf640e025b1afc0e4835c048eda26fc264
leejieun <mariewldms@naver.com>
2025-03-03 01:44:53 +0900

    Feat: AddressBook

--- src/test/java/org/spring/protobuf/ProtobufApplicationTests.java
+++ src/test/java/org/spring/protobuf/ProtobufApplicationTests.java
@@ -3,15 +3,20 @@ package org.spring.protobuf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 @SpringBootTest
 class ProtobufApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
 		String email="jieun@sookmyung.ac.kr";
 		int id=22;
 		String name="jieun lee";
 
+		//person 객체 생성
 		PersonProto.Person person=
 				PersonProto.Person.newBuilder()
 						.setId(id)
@@ -19,11 +24,29 @@ class ProtobufApplicationTests {
 						.setEmail(email)
 						.build();
 
-		System.out.println("-------");
-		System.out.println(person.getId());
-		System.out.println(person.getName());
-		System.out.println(person.getEmail());
-		System.out.println("-------");
+		//AddressBook 객체 생성
+		PersonProto.AddressBook addressBook
+				= PersonProto.AddressBook.newBuilder()
+						.addPeople(person)
+						.build();
+
+		String filePath="C:/Users/marie/Documents/addressBook.bin";
+
+		//직렬화
+		FileOutputStream fos = new FileOutputStream(filePath);
+		addressBook.writeTo(fos);
+		fos.close();
+
+		//역직렬화
+		FileInputStream fis = new FileInputStream(filePath);
+		PersonProto.AddressBook deserialized = PersonProto.AddressBook.parseFrom(fis);
+		fis.close();
+
+		for (PersonProto.Person p : deserialized.getPeopleList()) {
+			System.out.println("Name: "+ p.getName());
+			System.out.println("Id: " + p.getId());
+			System.out.println("Email: " + p.getEmail());
+		}
 
 	}
 
